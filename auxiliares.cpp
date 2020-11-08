6afc4668e0f72da9d52503d7574d3e1c360e142c
Gian <gianfrancobtl@gmail.com>
2020-11-08 18:30:59 -0300

    TPI final

---
+++ auxiliares.cpp
@@ -0,0 +1,13 @@
+#include "definiciones.h"
+#include "auxiliares.h"
+
+using namespace std;
+// aqui se pueden ubicar todas las funciones auxiliares de soporte para la resolucion de los ejercicios
+pair<int,int> mp(int a, int b) {
+    return make_pair(a, b);
+}
+
+vector<posicion> ordenar(vector<posicion> &v) {
+    sort(v.begin(), v.end());
+    return v;
+}
---
+++ ejercicios.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include "ejercicios.h"
+
+// EJERCICIO 1
+bool toroideValido(vector<vector<bool>> const &t) {
+    bool resp = false; // este valor puede ser cambiado de acuerdo a la propia implementacion
+    // Implementacion
+    return resp;
+}
+
+// EJERCICIO 2
+vector<posicion> posicionesVivas(toroide const &t) {
+	vector<posicion> vivos;
+    // Implementacion
+    return vivos;
+}
+
+// EJERCICIO 3
+float densidadPoblacion(toroide const &t) {
+	float resp = -1;
+    // Implementacion
+    return resp;
+}
+
+// EJERCICIO 4
+bool evolucionDePosicion(toroide const &t, posicion x) {
+	bool resp = false;
+    // Implementacion
+    return resp;
+}
+
+// EJERCICIO 5
+void evolucionToroide(toroide &t){
+    // Implementacions
+    return;
+}
+
+// EJERCICIO 6
+toroide evolucionMultiple(toroide const &t, int K) {
+    toroide out;
+    // Implementacion
+    return out;
+}
+
+// EJERCICIO 7
+bool esPeriodico(toroide const &t, int &p) {
+    bool resp = false;
+    // Implementacion
+    return resp;
+}
+
+// EJERCICIO 8
+bool primosLejanos(toroide const &t, toroide const &u) {
+	bool resp = false; 
+    // Implementacion
+    return resp;
+}
+
+// EJERCICIO 9
+int seleccionNatural(vector <toroide> ts) {
+    int resp = -1; // este valor puede ser cambiado de acuerdo a la propia implementacion    
+	// Implementacion
+    return resp;
+}
+
+// EJERCICIO 10
+toroide fusionar(toroide const &t, toroide const &u) {
+    toroide out;
+    // Implementacion
+    return out;
+}
+
+// EJERCICIO 11
+bool vistaTrasladada(toroide const &t, toroide const &u){
+	bool resp = false;
+    // Implementacion
+    return resp;
+}
+
+// EJERCICIO 12
+int menorSuperficieViva(toroide const &t){
+	int resp = -1;
+	// Implementacion
+	return resp;
+}
