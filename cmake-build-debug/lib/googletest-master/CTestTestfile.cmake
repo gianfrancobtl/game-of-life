e7be4b8df9827be47eaf4f5c97b04a8bdb9d7e93
Gian <gianfrancobtl@gmail.com>
2020-11-24 15:03:51 -0300

    Borro debug folder.

--- cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gianbattolla/Documents/tpi_final/lib/googletest-master
-# Build directory: /Users/gianbattolla/Documents/tpi_final/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gianbattolla/Documents/tpi_final/lib/googletest-master/googletest
-# Build directory: /Users/gianbattolla/Documents/tpi_final/cmake-build-debug/lib/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
